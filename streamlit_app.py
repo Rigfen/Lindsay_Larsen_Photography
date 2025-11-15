5e44e11e23df355843dde5a72575f992eb872312
Rigfen <Rigfen@gmail.com>
2025-11-15 15:20:06 -0800

    Refactor Streamlit app layout and photo upload feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
-import streamlit as st
+import Streamlit as st 
+import pandas as pd
+import math
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+col1, col2, col3 = st. colums ([1,2,1])
+with col2:
+    st.title("Photo Me")
+
+st.markdown(
+
 )
+st.subheader("Upload photo,\n Choose a file\n Choose a Photo")
+
+uploaded_file = st.file_uploader("Upload a Photo", type=["jpg","png","jpeg"])
+
+if uploaded_file:
+    st.image(uploaded_file)
