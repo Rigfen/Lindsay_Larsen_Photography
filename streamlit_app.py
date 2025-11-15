199991ac017d00cc451349c9ad1b2409e1e2cb8a
Rigfen <Rigfen@gmail.com>
2025-11-15 15:28:04 -0800

    Refactor title and markdown display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,14 @@ import pandas as pd
 import math
 import os
 
-col1, col2, col3 = st.columns ([1,2,1])
-with col2:
-    st.title("Photo Me")
+st.title("Photo Me")
 
-st.markdown(
+st.markdown("_________")
 
-)
 st.subheader("Upload photo,\n Choose a file\n Choose a Photo")
 
 uploaded_file = st.file_uploader("Upload a Photo", type=["jpg","png","jpeg"])
 
 if uploaded_file:
     st.image(uploaded_file)
+st.button
